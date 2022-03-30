02d2fc3caa64251cff46ad6f439acaefeb23ec4a
CesarDiempi <c.tejero@diempi.com>
2022-03-30 15:10:13 +0200

    Final proyecto: Subiendo pagina web

--- Proyecto_ASPNETCoreWebApi/wwwroot/js/site.js
+++ Proyecto_ASPNETCoreWebApi/wwwroot/js/site.js
@@ -1,5 +1,5 @@
-﻿const uri = 'api/todoitems';
-let todos = [];
+﻿const uri = 'api/departments';
+let departments = [];
 
 function getItems() {
     fetch(uri)
@@ -10,10 +10,11 @@ function getItems() {
 
 function addItem() {
     const addNameTextbox = document.getElementById('add-name');
+    const addGroupTextbox = document.getElementById('add-group');
 
-    const item = {
-        isComplete: false,
-        name: addNameTextbox.value.trim()
+    const item = {        
+        name: addNameTextbox.value.trim(),
+        groupName: addGroupTextbox.value.trim(),        
     };
 
     fetch(uri, {
@@ -28,6 +29,7 @@ function addItem() {
         .then(() => {
             getItems();
             addNameTextbox.value = '';
+            addGroupTextbox.value = '';
         })
         .catch(error => console.error('Unable to add item.', error));
 }
@@ -41,20 +43,20 @@ function deleteItem(id) {
 }
 
 function displayEditForm(id) {
-    const item = todos.find(item => item.id === id);
+    const item = departments.find(item => item.departmentId === id);
 
     document.getElementById('edit-name').value = item.name;
-    document.getElementById('edit-id').value = item.id;
-    document.getElementById('edit-isComplete').checked = item.isComplete;
+    document.getElementById('edit-id').value = item.departmentId;
+    document.getElementById('edit-group').value = item.groupName;
     document.getElementById('editForm').style.display = 'block';
 }
 
 function updateItem() {
     const itemId = document.getElementById('edit-id').value;
     const item = {
-        id: parseInt(itemId, 10),
-        isComplete: document.getElementById('edit-isComplete').checked,
-        name: document.getElementById('edit-name').value.trim()
+        departmentId: parseInt(itemId, 10),        
+        name: document.getElementById('edit-name').value.trim(),
+        groupName: document.getElementById('edit-group').value.trim()
     };
 
     fetch(`${uri}/${itemId}`, {
@@ -78,41 +80,39 @@ function closeInput() {
 }
 
 function _displayCount(itemCount) {
-    const name = (itemCount === 1) ? 'to-do' : 'to-dos';
+    const name = (itemCount === 1) ? 'department' : 'departments';
 
     document.getElementById('counter').innerText = `${itemCount} ${name}`;
 }
 
 function _displayItems(data) {
-    const tBody = document.getElementById('todos');
+    const tBody = document.getElementById('departments');
     tBody.innerHTML = '';
 
     _displayCount(data.length);
 
     const button = document.createElement('button');
 
-    data.forEach(item => {
-        let isCompleteCheckbox = document.createElement('input');
-        isCompleteCheckbox.type = 'checkbox';
-        isCompleteCheckbox.disabled = true;
-        isCompleteCheckbox.checked = item.isComplete;
-
+    data.forEach(item => {       
         let editButton = button.cloneNode(false);
         editButton.innerText = 'Edit';
-        editButton.setAttribute('onclick', `displayEditForm(${item.id})`);
+        editButton.setAttribute('onclick', `displayEditForm(${item.departmentId})`);
+
+        //console.log(item.departmentId);
 
         let deleteButton = button.cloneNode(false);
         deleteButton.innerText = 'Delete';
-        deleteButton.setAttribute('onclick', `deleteItem(${item.id})`);
+        deleteButton.setAttribute('onclick', `deleteItem(${item.departmentId})`);
 
         let tr = tBody.insertRow();
 
         let td1 = tr.insertCell(0);
-        td1.appendChild(isCompleteCheckbox);
+        let textNodeName = document.createTextNode(item.name);
+        td1.appendChild(textNodeName);
 
         let td2 = tr.insertCell(1);
-        let textNode = document.createTextNode(item.name);
-        td2.appendChild(textNode);
+        let textNodeGroup = document.createTextNode(item.groupName);
+        td2.appendChild(textNodeGroup);
 
         let td3 = tr.insertCell(2);
         td3.appendChild(editButton);
@@ -121,5 +121,5 @@ function _displayItems(data) {
         td4.appendChild(deleteButton);
     });
 
-    todos = data;
+    departments = data;
 }
